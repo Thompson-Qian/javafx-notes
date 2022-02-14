f5d47d4a97e071705ff995b770d7d7c6e8c896f9
Thompson-Qian <931365716@qq.com>
2022-02-14 14:43:41 +0000

    23333333333333

--- full-stack appliaction/javaFX/week2/ToDoList2/src/main/java/fsd/week2/todolist2/Main.java
+++ full-stack appliaction/javaFX/week2/ToDoList2/src/main/java/fsd/week2/todolist2/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class Main extends Application {//2333333333333333333333333333333333
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-window-view.fxml"));
